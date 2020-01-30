2d0499e6a66dca522f1cb10752ee2ba6f12eea0b
BlackBloodE <106021032@live.asia.edu.tw>
2020-01-31 01:41:37 +0800

    筆電端更新

--- app/src/main/java/com/BlackBloodE/WaTaShiWaNo1/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/BlackBloodE/WaTaShiWaNo1/ui/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.BlackBloodE.WaTaShiWaNo1.ui.settings
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +20,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.BlackBloodE.WaTaShiWaNo1.R
+import org.json.JSONException
+import org.json.JSONObject
+import org.jsoup.parser.Parser
+import java.net.URL
 
 
 class SettingsFragment : Fragment() {
@@ -69,21 +76,46 @@ class SettingsFragment : Fragment() {
                     Toast.makeText(context, "您點選了 " + str[position] + " 但此功能尚未實現，不好意思", Toast.LENGTH_SHORT).show()
                 }
                 2 ->{
-                    val fileName = Uri.parse("https://github.com/BlackBloodE/WaTaShiWaNo1/releases/latest")
-                    val intent = Intent()
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.action = Intent.ACTION_VIEW
-                    intent.setData(fileName)
-                    startActivity(intent)
+                    Thread{
+                        var netVer = getNewVer()
+                        var strUp = "需要更新!!"
+                        var strNUP = "無須更新"
+                        var ADt = ""
+                        if (getLocalVersion(context!!)!=netVer){
+                            ADt = strUp
+                        }else{
+                            ADt = strNUP
+                        }
+                        Looper.prepare()
+                        AlertDialog.Builder(context)
+                            .setMessage("目前版本:"+getLocalVersion(context!!)+"\n"+"最新版本:"+netVer)
+                            .setTitle("檢查更新..."+ADt)
+                            .setPositiveButton("取得最新版本", DialogInterface.OnClickListener { _, _ ->
+                                val fileName = Uri.parse("https://github.com/BlackBloodE/WaTaShiWaNo1/releases/latest")
+                                val intent = Intent()
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                intent.action = Intent.ACTION_VIEW
+                                intent.setData(fileName)
+                                startActivity(intent)
+                            })
+                            .setNeutralButton("取消", null)
+                            .create()
+                            .show()
+                        Looper.loop()
+                    }.start()
+//                    val fileName = Uri.parse("https://github.com/BlackBloodE/WaTaShiWaNo1/releases/latest")
+//                    val intent = Intent()
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                    intent.action = Intent.ACTION_VIEW
+//                    intent.setData(fileName)
+//                    startActivity(intent)
                 }
                 3 ->{
                     Toast.makeText(context, "您點選了 " + str[position] + " 但此功能尚未實現，不好意思", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-    /**
-     * 获取本地软件版本名
-     */
+    //獲取版本號
     fun getLocalVersion(ctx: Context): String {
         var localVersion = ""
         try {
@@ -96,5 +128,25 @@ class SettingsFragment : Fragment() {
             e.printStackTrace()
         }
         return localVersion
+    }
+    //解析GitHub Release的json
+    fun getNewVer() : String {
+        var netVer = ""
+
+        val result = URL("https://api.github.com/repos/BlackBloodE/WaTaShiWaNo1/releases/latest").readText()
+        val json: JSONObject = parse(result) as JSONObject
+        netVer = json.getString("tag_name")
+        //println(netVer)
+
+        return netVer
+    }
+    fun parse(json: String): JSONObject? {
+        var jsonObject: JSONObject? = null
+        try {
+            jsonObject = JSONObject(json)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        return jsonObject
     }
 }
